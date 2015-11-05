d0d1432451624fea813e14673a285b961a3fe881
cgarconnet <cgarconnet@gmail.com>
2015-11-05 22:51:54 +0100

    try without success on ETE library

--- partner.py
+++ partner.py
@@ -16,6 +16,28 @@ class Partner():
 	# 		ret += child.__repr__(level+1)
 	# 	return ret
 
+	def __iter__(self):
+#        """ return the iterator that iterates through the elements in the BST 
+#        rooted at this node in an inorder sequence """
+        
+		if self.left:
+            # The following iterates through all the nodes in the left subtree. 
+            
+            # The first thing that python does when the for loop is encountered
+            # is to obtain an iterator object for the left subtree.  
+            # This is done ("under the covers") by recursively calling 
+            # the __iter__ method on the left child. 
+			for elt in self.left:         
+				yield elt
+                
+        # at this point we "visit" the current node
+		yield self.rootid
+        
+		if self.right:
+            # we now visit all the nodes in the right subtree 
+			for elt in self.right:
+				yield elt
+
 	def getLeftChild(self):
 		return self.left
 	def getRightChild(self):
@@ -33,12 +55,12 @@ class Partner():
 			self.right.find(x) #, level+1
 		return "I founded: " + self.rootid if self.rootid == x else x + " not founded"
 
-	def BFT(node):
+	def BFT(tree):
 
-	    node.level = 1
-	    queue = deque([node])
+	    tree.level = 1
+	    queue = deque([tree])
 	    output = []
-	    current_level = node.level
+	    current_level = tree.level
 
 	    while len(queue)>0:
 
@@ -61,6 +83,43 @@ class Partner():
 	                 
 	 
 	    return ''.join(output)
+
+
+	def BFT_offset(tree, offset):
+
+	    tree.level = 1
+	    queue = deque([tree])
+	    print queue
+	    output = []
+	    found = False
+	    current_level = tree.level
+#	    print offset
+
+	    while len(queue)>0:
+
+	        current_node = queue.popleft()
+	    	print current_node	        
+#	        	found = True
+
+	        if(current_node.level > current_level):
+	            output.append("\n")
+	            current_level += 1
+
+	        if str(current_node) == str(offset) or found:
+		        output.append(str(current_node))
+		        found = True
+
+	        if current_node.left != None:
+	            current_node.left.level = current_level + 1 
+	            queue.append(current_node.left) 
+
+	        if current_node.right != None:
+	            current_node.right.level = current_level + 1 
+	            queue.append(current_node.right)
+                
+	 
+	    return ''.join(output)
+
 
 	def insertRight(self,newNode):
 		if self.right == None:
@@ -107,7 +166,10 @@ def testTree():
 	myTree.insertRight("Steven")
 	myTree.insertLeft("Matt")
 	printTree(myTree)
+	print "---"
 	print myTree.BFT()	
+	print "---"
+	print myTree.BFT_offset("Tony")	
 	# print('1st research')
 	# print(myTree.find("Maud"))
 	# print('2nd research')
